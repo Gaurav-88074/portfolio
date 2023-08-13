adf3b0525008915a14ea67b655a60189cdcfec86
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-13 13:49:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with gaurav and rohit
+          Learn React with chat gpt
         </a>
       </header>
     </div>
