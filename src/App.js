4e43b8f107310769194148ab86958c4b9b3bfdb3
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-13 14:03:04 +0530

    Gaurav

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with chat gpt
+          dono ka chal rha hai abb
         </a>
       </header>
     </div>
