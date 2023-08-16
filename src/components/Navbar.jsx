26a53c4d28834096c911b933f5322402bf2b1a24
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-16 18:13:08 +0530

    Added : night mode button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,74 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import { useState } from "react";
 const Navbar = () => {
+    const [lightMode, setLightMode] = useState(true);
     return (
         <nav className={styles.navbar}>
             <div className={styles.navContainer}>
-                Gaurav
+                <a>Gaurav</a>
             </div>
             <div className={styles.menu}>
                 <ul className={styles.submenu}>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Services</li>
-                    <li>Contact Me</li>
+                    <li>
+                        <a>Home</a>
+                    </li>
+                    <li>
+                        <a>About</a>
+                    </li>
+                    <li>
+                        <a>Skills</a>
+                    </li>
+                    <li>
+                        <a>Projects</a>
+                    </li>
+                    <li>
+                        <a>Contact Me</a>
+                    </li>
                 </ul>
             </div>
-            <div className={styles.navButton}>
-                <button>
-                    click
-                </button>
+            <div className={styles.navButton}
+                onClick={()=>{
+                    setLightMode((p)=>!p);
+                }}
+            >
+                {
+                    lightMode
+                    &&
+                    <DarkModeOutlinedIcon
+                        sx={{
+                            color: "#716b74",
+                            fontSize :'1.3rem',
+                            backgroundColor: "#fbfbfe",
+
+                            ":hover": {
+                                color: "#6f58df",
+                                cursor: "pointer",
+                            },
+                            // border :
+                        }}
+                        
+                    />
+                }
+                {
+                    !lightMode
+                    &&
+                    <LightModeOutlinedIcon
+                    sx={{
+                        color: "#716b74",
+                        fontSize :'1.3rem',
+                        backgroundColor: "#fbfbfe",
+
+                        ":hover": {
+                            color: "#6f58df",
+                            cursor: "pointer",
+                        },
+                        // border :
+                    }}
+                    />
+                }
             </div>
         </nav>
     );
